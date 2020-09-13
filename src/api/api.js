028addd475ff62f24636ec6c23eac1c53b2b6ca6
monacook <cook.mona@gmail.com>
2020-09-12 18:06:30 -0700

    organized ap structure

--- src/api/api.js
+++ src/api/api.js
@@ -1,36 +1,35 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-class api extends React.Component {
-   constructor(){
-    super();
-    this.state= {
-      error: null,
-      isLoaded: false,
-      updated: []
-    };
-  }  
+// class api extends React.Component {
+//    constructor(){
+//     super();
+//     this.state= {
+//       error: null,
+//       isLoaded: false,
+//       updated: []
+//     };
+//   }  
 
-    componentDidMount() {
-    axios({
-      "method":"GET",
-      "url":"https://coronavirus-monitor-v2.p.rapidapi.com/coronavirus/cases_in_united_states_worldometers_latest.php",
-      "headers":{
-      "content-type":"application/octet-stream",
-      "x-rapidapi-host":"coronavirus-monitor-v2.p.rapidapi.com",
-      "x-rapidapi-key":"***************************************"
-          }
-        })
-         .then((response)=>{
-           this.setState({ updated: response.data });
-            // console.log(this.state);
-          })
-          .catch((error)=>{
-            console.log(error)
-          })
-      }
+//     componentDidMount() {
+//     axios({
+//       "method":"GET",
+//       "url":"https://coronavirus-monitor-v2.p.rapidapi.com/coronavirus/cases_in_united_states_worldometers_latest.php",
+//       "headers":{
+//       "content-type":"application/octet-stream",
+//       "x-rapidapi-host":"coronavirus-monitor-v2.p.rapidapi.com",
+//       "x-rapidapi-key":"***************************************"
+//           }
+//         })
+//          .then((response)=>{
+//            this.setState({ updated: response.data });
+//             // console.log(this.state);
+//           })
+//           .catch((error)=>{
+//             console.log(error)
+//           })
+//       }
       
-      render() {
+//       render() {
 
-      }
+//       }
 
-      
