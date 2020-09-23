72775ea0aea58ac7fe8061a3d3f0fbaa2eaadde8
monacook <cook.mona@gmail.com>
2020-09-23 14:17:13 -0700

    Updates

---
+++ precache-manifest.971b71145c9bffa4c161ccbd49628165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa90a0b73323ca2b04896203aae8c475",
+    "url": "/Covid.Tracker/index.html"
+  },
+  {
+    "revision": "7ea50b84912784b897eb",
+    "url": "/Covid.Tracker/static/css/main.179a250b.chunk.css"
+  },
+  {
+    "revision": "9a98649e57a9c30f8026",
+    "url": "/Covid.Tracker/static/js/2.e863e9c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid.Tracker/static/js/2.e863e9c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea50b84912784b897eb",
+    "url": "/Covid.Tracker/static/js/main.d50cbbd8.chunk.js"
+  },
+  {
+    "revision": "41d82a039f01bc279ebd",
+    "url": "/Covid.Tracker/static/js/runtime-main.e11e4021.js"
+  }
+]);
