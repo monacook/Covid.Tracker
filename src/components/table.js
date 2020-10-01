787cd920b514fa295faae3fbd3ac1efa245c55dd
Pandu <pandurijal96@gmail.com>
2020-10-01 08:59:49 +0700

    implement number format function on table

--- src/components/table.js
+++ src/components/table.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tbody, Tr, Td } from 'react-super-responsive-table';
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import { numberFormatting } from "../helpers/number";
 
 export default class Tables extends React.Component {   
     render() {
@@ -8,14 +9,14 @@ export default class Tables extends React.Component {
         <Tbody> 
             <Tr>
                 <Td>{this.props.name}</Td>
-                <Td>{this.props.newCases}</Td>
-                <Td>{this.props.critical}</Td>
-                <Td>{this.props.active}</Td>
-                <Td>{this.props.recovered}</Td>
-                <Td>{this.props.new}</Td>
-                <Td>{this.props.deathTotal}</Td>
-                <Td>{this.props.casesTotal}</Td>
-                <Td>{this.props.testTotal}</Td>
+                <Td>{numberFormatting(this.props.newCases)}</Td>
+                <Td>{numberFormatting(this.props.critical)}</Td>
+                <Td>{numberFormatting(this.props.active)}</Td>
+                <Td>{numberFormatting(this.props.recovered)}</Td>
+                <Td>{numberFormatting(this.props.new)}</Td>
+                <Td>{numberFormatting(this.props.deathTotal)}</Td>
+                <Td>{numberFormatting(this.props.casesTotal)}</Td>
+                <Td>{numberFormatting(this.props.testTotal)}</Td>
             </Tr>
         </Tbody> 
         )
