28f1e0aaeefa420cdd5ba6fc60f666b14c5541d3
monacook <cook.mona@gmail.com>
2020-10-26 12:32:03 -0700

    what is this

---
+++ src/components/table 2.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Tbody, Tr, Td } from 'react-super-responsive-table';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import { numberFormatting } from "../helpers/number";
+import moment from 'moment';
+
+
+export default class Tables extends React.Component {   
+    render() {
+        return (
+        <Tbody> 
+            <Tr>
+                <Td>{this.props.name}</Td>
+                <Td>{numberFormatting(this.props.newCases)}</Td>
+                <Td>{numberFormatting(this.props.critical)}</Td>
+                <Td>{numberFormatting(this.props.active)}</Td>
+                <Td>{numberFormatting(this.props.recovered)}</Td>
+                <Td>{numberFormatting(this.props.new)}</Td>
+                <Td>{numberFormatting(this.props.deathTotal)}</Td>
+                <Td>{numberFormatting(this.props.casesTotal)}</Td>
+                <Td>{numberFormatting(this.props.testTotal)}</Td>
+                <Td>{moment(this.props.lastUpdated).format('lll')}</Td>
+            </Tr>
+        </Tbody> 
+        )
+    }
+}
