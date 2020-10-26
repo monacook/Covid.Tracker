7b53fe3855544696a95b05133f6cf000938d60df
monacook <cook.mona@gmail.com>
2020-10-26 12:46:42 -0700

    stuff

---
+++ build/precache-manifest.cf44535a215afaf1e8c6aaaab5e9a552.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990af0ff96ada8d8cf250a6eacad4896",
+    "url": "/Covid.Tracker/index.html"
+  },
+  {
+    "revision": "60774a30372e835e4eb1",
+    "url": "/Covid.Tracker/static/css/2.326c04ff.chunk.css"
+  },
+  {
+    "revision": "66cc91185ab988811e60",
+    "url": "/Covid.Tracker/static/css/main.908dc4c4.chunk.css"
+  },
+  {
+    "revision": "60774a30372e835e4eb1",
+    "url": "/Covid.Tracker/static/js/2.04c59906.chunk.js"
+  },
+  {
+    "revision": "c53f65871ab74006a0c4e0b92b224267",
+    "url": "/Covid.Tracker/static/js/2.04c59906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66cc91185ab988811e60",
+    "url": "/Covid.Tracker/static/js/main.c3bcfef8.chunk.js"
+  },
+  {
+    "revision": "41d82a039f01bc279ebd",
+    "url": "/Covid.Tracker/static/js/runtime-main.e11e4021.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0ed12d31a42a4257138442e9e059b5e.js"
+  "/Covid.Tracker/precache-manifest.cf44535a215afaf1e8c6aaaab5e9a552.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Covid.Tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
