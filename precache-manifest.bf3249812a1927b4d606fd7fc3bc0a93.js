612c289530699e95afac760c0149592ceab8f1f3
monacook <cook.mona@gmail.com>
2020-10-26 13:02:30 -0700

    Updates

---
+++ precache-manifest.bf3249812a1927b4d606fd7fc3bc0a93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005cc8f8114810b141c2b8cbe00b6e66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60774a30372e835e4eb1",
+    "url": "/static/css/2.326c04ff.chunk.css"
+  },
+  {
+    "revision": "cd182c4a3175f648ea82",
+    "url": "/static/css/main.908dc4c4.chunk.css"
+  },
+  {
+    "revision": "60774a30372e835e4eb1",
+    "url": "/static/js/2.04c59906.chunk.js"
+  },
+  {
+    "revision": "c53f65871ab74006a0c4e0b92b224267",
+    "url": "/static/js/2.04c59906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd182c4a3175f648ea82",
+    "url": "/static/js/main.b36fc44a.chunk.js"
+  },
+  {
+    "revision": "b2cd27896d16b3942c02",
+    "url": "/static/js/runtime-main.d2d99100.js"
+  }
+]);
